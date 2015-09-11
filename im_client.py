6e3407319f35d83d45c61ec2dda8fbe64d06d72c
micafer <micafer1@upv.es>
2015-09-11 13:16:38 +0200

    Set version 1.3.2

--- im_client.py
+++ im_client.py
@@ -23,7 +23,7 @@ import tempfile
 from optparse import OptionParser, Option, IndentedHelpFormatter
 import ConfigParser
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 class PosOptionParser(OptionParser):
 	def format_help(self, formatter=None):
